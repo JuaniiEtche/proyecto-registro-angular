b9cfde8947b6bb30269a5b86f320340c30d6d8ae
dolodmg <oranotherway@gmail.com>
2023-11-08 16:16:22 -0300

    Feature: Editar datos personales

--- src/app/components/becario-datos-personales/becario-datos-personales.component.ts
+++ src/app/components/becario-datos-personales/becario-datos-personales.component.ts
@@ -8,13 +8,21 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./becario-datos-personales.component.css'],
 })
 export class BecarioDatosPersonalesComponent {
-  resultado: any[] | undefined = undefined;
-
-  nombreApellido: any;
+  idPersona: any;
+  nombre: any;
+  apellido: any;
   dni: any;
-  telefono: any;
-  legajo: any;
+  numTelefono: any;
   email: any;
+  legajo: any;
+  nombreOriginal: any;
+  apellidoOriginal: any;
+  dniOriginal: any;
+  numTelefonoOriginal: any;
+  emailOriginal: any;
+  legajoOriginal: any;
+  resultado: any[] | undefined = undefined;
+  editandoPersonales: boolean = false;
 
   constructor(
     private registroLinsiService: RegistroLinsiService,
@@ -23,21 +31,73 @@ export class BecarioDatosPersonalesComponent {
 
   ngOnInit() {
     this.route.queryParams.subscribe((params) => {
-      const idPersona = params['idPersona'];
-      this.armarDatosPersonales(idPersona);
+      this.idPersona = params['idPersona'];
+      this.cargarDatosPersonales(this.idPersona);
     });
   }
 
-  async armarDatosPersonales(idPersona: number): Promise<void> {
+  async cargarDatosPersonales(idPersona: number): Promise<void> {
     try {
       const response: any =
-        await this.registroLinsiService.cargarDatosPersonales(idPersona);
+      await this.registroLinsiService.cargarDatosPersonales(idPersona);
       this.resultado = response;
-      this.nombreApellido = response.nombre + ' ' + response.apellido;
+      this.nombre = response.nombre;
+      this.apellido = response.apellido;
       this.dni = response.dni;
       this.legajo = response.legajo;
+      this.numTelefono = response.numTelefono;
       this.email = response.email;
-      this.telefono = response.numTelefono;
+      this.nombreOriginal = response.nombre;
+      this.apellidoOriginal = response.apellido;
+      this.dniOriginal = response.dni;
+      this.legajoOriginal = response.legajo;
+      this.numTelefonoOriginal = response.numTelefono;
+      this.emailOriginal = response.email;
     } catch (error) {}
+  }
+
+  cancelarEdicionDatosPersonales() {
+    this.nombre = this.nombreOriginal;
+    this.apellido = this.apellidoOriginal;
+    this.dni = this.dniOriginal;
+    this.legajo = this.legajoOriginal;
+    this.numTelefono = this.numTelefonoOriginal;
+    this.email = this.emailOriginal;
+    this.editandoPersonales = false;
+  }
+
+  editarDatosPersonales() {
+    if (this.editandoPersonales) {
+      this.guardarDatosPersonales();
+      this.editandoPersonales = false;
+    } else {
+      // Activa el modo de edición
+      this.editandoPersonales = true;
+    }
+  }
+
+  async guardarDatosPersonales() {
+    try {
+      const response = await this.registroLinsiService.actualizarDatosPersonales(
+        this.idPersona,
+        this.nombre,
+        this.apellido,
+        this.dni,
+        this.numTelefono,
+        this.email,
+        this.legajo
+      );
+      console.log(response);
+      if (response && response.exitoso) {
+        console.log('Datos personales actualizados con éxito');
+      } else {
+        console.error('Error');
+      }
+    } catch (error) {
+      console.error('Error al actualizar datos personales', error);
+    }
+  
+
+    this.editandoPersonales = false;
   }
 }
